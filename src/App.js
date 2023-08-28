59829719b929d3df7a79f33048d3959b0c4f2f86
manya <manyaraghuvanshi66@gmail.com>
2023-08-28 15:57:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import IpTracker from "./IpTracker";
+import "leaflet/dist/leaflet.css";
+// import "mapbox-gl/dist/mapbox-gl.css";
+// import * as React from 'react';
+// import Map from 'react-map-gl';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <IpTracker />
+
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </>
+  )
 }
 
 export default App;
